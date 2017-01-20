a1fdc9c71e80c2b69692a5251785b9e8c1505c78
Steven Cable <steven.cable12@gmail.com>
2017-01-19 18:40:05 -1000

    all pau

--- todo_app.sql
+++ todo_app.sql
@@ -23,6 +23,31 @@ ALTER TABLE tasks
   ALTER COLUMN updated_at SET DEFAULT now();
 
 INSERT INTO "tasks" (title, description, created_at, updated_at, completed_at) VALUES ('Study SQL', 'Complete this exercise', now(), now(), NULL);
+
+INSERT INTO "tasks" (title, description) VALUES ('Study PostgreSQL', 'Read all the documentation');
+
+SELECT title FROM "tasks" WHERE completed_at IS NULL;
+
+UPDATE "tasks" SET completed_at = now() WHERE title = 'Study PostgreSQL';
+
+SELECT title, description FROM "tasks" WHERE completed_at IS NULL;
+
+SELECT * FROM "tasks" ORDER BY created_at DESC;
+
+INSERT INTO "tasks" (title, description) VALUES ('mistake 1', 'another test entry');
+
+INSERT INTO "tasks" (title, description) VALUES ('mistake 2', 'another test entry');
+
+INSERT INTO "tasks" (title, description) VALUES ('third mistake', 'another test entry');
   
-SELECT * FROM "tasks";
+SELECT title FROM "tasks" WHERE title LIKE '%mistake%';
+
+DELETE FROM "tasks" WHERE title = 'mistake 1';
+
+SELECT title, description FROM "tasks" WHERE title LIKE '%mistake%';
+
+DELETE FROM "tasks" WHERE title LIKE '%mistake%';
+
+SELECT * FROM "tasks" ORDER BY title ASC;
+
 \d+ "tasks";
